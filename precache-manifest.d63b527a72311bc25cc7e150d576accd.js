54e1f57a776abf294dc26fac790fba3d045e449b
ron <wanko@digitwebsolution.com>
2020-06-13 16:28:33 -0500

    Updates

---
+++ precache-manifest.d63b527a72311bc25cc7e150d576accd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3422c317107c40635e0a753d09ee4e77",
+    "url": "/reactResto/index.html"
+  },
+  {
+    "revision": "0cc1dd4aa172b885ca8a",
+    "url": "/reactResto/static/css/main.bf7aa4f6.chunk.css"
+  },
+  {
+    "revision": "0e4046bb78b1362f3132",
+    "url": "/reactResto/static/js/2.81b96aa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactResto/static/js/2.81b96aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cc1dd4aa172b885ca8a",
+    "url": "/reactResto/static/js/main.34b3d9dc.chunk.js"
+  },
+  {
+    "revision": "0ad547421a6ca374816e",
+    "url": "/reactResto/static/js/runtime-main.97ef8fba.js"
+  },
+  {
+    "revision": "6907da6abd9cc67492f35634ebe829e6",
+    "url": "/reactResto/static/media/burger1.6907da6a.jpg"
+  },
+  {
+    "revision": "9b4bee4243bcc43221f1fb7599bc3e8a",
+    "url": "/reactResto/static/media/prdc1.9b4bee42.jpg"
+  },
+  {
+    "revision": "4d5401b23143b6fa17fda5821852c80d",
+    "url": "/reactResto/static/media/prdc2.4d5401b2.jpg"
+  },
+  {
+    "revision": "d6bf94fca56f390a55cf945b9671498e",
+    "url": "/reactResto/static/media/prdc3.d6bf94fc.jpg"
+  },
+  {
+    "revision": "026b52bfabf394f131c24dbd522aefb3",
+    "url": "/reactResto/static/media/steak1.026b52bf.jpg"
+  }
+]);
